641b6e75df6fd31be389f5ebee8c120a940b5342
DamienHuet <damien.huet@protonmail.com>
2018-05-14 19:35:44 +0200

    Mapping function seems to be working. Debugged the error due to the lack of terminate condition in the while loops.

--- main.c
+++ main.c
@@ -3,21 +3,61 @@
 #include <stdlib.h>
 #include "user_case.h"
 
+// Relevant macros for the working program
+#define FILE_RMODE (MPI_MODE_RDONLY)
+
+//  Relevant macros for debugging purposes
+#define DEBUG_ON_MASTER
+//Max string length we allow the master to read for debugging purposes
+#define DEBUG_LEN_STR 10
+// Debugging on the small test file
+#define FILE_NAME "../test_files/wikipedia_test_small.txt"
+
 int main(int argc, char** argv){
     MPI_Init(&argc, &argv);
     //get rank or other head operations
-    
+    MPI_File fh;
+    int rank, num_ranks;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+
     /***map and reduce phase***/
-    
-    while(){        //file is not drain after this draw
+    if (rank==0)
+    {
+      MPI_Offset file_size_bytes;   // Apparently MPI_Offset is just a long long int (%lld)
+      MPI_File_open(MPI_COMM_SELF, FILE_NAME, FILE_RMODE,MPI_INFO_NULL,&fh);
+      //--- It would be smart to include a test in case we fail to open the file ---//
+      MPI_File_get_size(fh, &file_size_bytes); //Length of the file in bytes
+
+      int task_len=DEBUG_LEN_STR;
+      if (task_len>file_size_bytes/sizeof(char)) task_len=file_size_bytes/sizeof(char);
+      char* task = (char*) malloc(task_len*sizeof(char));
+      MPI_File_read(fh,task,task_len,MPI_CHAR,MPI_STATUS_IGNORE);
+
+      for (int i=0;i<task_len;i++){
+        printf("%c",task[i]);
+      }
+      printf("\n");
+
+      KEYVAL word;
+      Map(task,task_len,&word);
+      printf("%s\n",word.key);
+
+      // Free the key, which was dynamically allocated
+      delete[] word.key;
+
+      MPI_File_close(&fh);
+    }
+
+      /*while(){        //file is not drain after this draw
         if(rank == 0){
             //reading from input file
         }
-        
+
         //using scatter(add invalid data in the front to send to root), or scatterv
         //root won't receive valid data
         //data type to be specified
-        
+
         if(rank != 0){
             //while task (file block is not drain)
                 //call map
@@ -25,22 +65,24 @@ int main(int argc, char** argv){
                 //call hash function to compute target rank
                 //store <key,value> into buckets for each rank
                 //remember checking redundancy
-        } 
+        }
         //data type to be specified
     }
-        
-    /*
-    till now, <key, value> should be stored in each process. 
-    They are orgainized according to target process in combine phase.   
-    */
+
+
+    //Till now, <key, value> should be stored in each process.
+    //They are orgainized according to target process in combine phase.
+
     //data type to be specified
-        
-    /***combine phase***/ 
+
+    //---- Combine phase
     //initialize arrays for all to all transmission
     //call all to all
-    
+
     //call reduce
-      
+
     //use gather to acquire result
+    */
     MPI_Finalize();
+    return(0);
 }
--- user_case.h
+++ user_case.h
@@ -3,7 +3,7 @@
 
 typedef struct{
   int key_len;
-  char key;
+  char* key;
   int val;
 } KEYVAL;
 
@@ -13,7 +13,8 @@ typedef struct{
 bool isLetter(char x)
 {
   bool isletter=0;
-  if (((int) x > 95) && ((int) x < 123 ) && ((int) x > 64) && ((int) x < 91)) isletter=1;
+  printf("%d\n", x);
+  if (((int) x > 95) && ((int) x < 123 ) || ((int) x > 64) && ((int) x < 91)) isletter=1;
   return(isletter);
 }
 
@@ -24,26 +25,30 @@ bool isDigit(char x)
   return(isdigit);
 }
 
-void Map(char* task,KEYVAL *word)
+KEYVAL Map(char* task, int task_len, KEYVAL *word)
 {
   int i=0;
-  while(!isDigit(task[i]) || !isLetter(task[i])) i++;
+  while(!isDigit(task[i]) && !isLetter(task[i]) && i<task_len)
+  {
+      // Add terminate condition (is last char is letter or digit, segfault!)
+      i++;
+  }
   if (isLetter(task[i]))
   {
     word->key_len=0;
-    while (isLetter(task[i+word->key_len])) word->key_len++;
-    word->key=malloc(word->key_len*sizeof(char));
+    while (isLetter(task[i+word->key_len]) && i+word->key_len<task_len) word->key_len++;
+    word->key =  new char[word->key_len];
     for(int j=0;j<word->key_len;j++) word->key[j]=task[i+j];
   }
   if (isDigit(task[i]))
   {
-    word.key_len=0;
-    while (isDigit(task[i+j])) word.key_len++;
-    word.key=malloc(word.key_len*sizeof(char));
-    for(int j=0;j<word->key_len;j++) word.key[j]=task[i+j];
+    word->key_len=0;
+    while (isDigit(task[i+word->key_len]) && i+word->key_len<task_len) word->key_len++;
+    word->key = new char[word->key_len];
+    for(int j=0;j<word->key_len;j++) word->key[j]=task[i+j];
   }
-  word.val=1;
-  return(word);
+  word->val=1;
+  return(*word);
 }
 
 #endif
