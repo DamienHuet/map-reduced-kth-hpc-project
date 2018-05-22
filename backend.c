b4dc47b3f373135af881aff0566f086299e82af0
weijiangk <kongweijiang@hotmail.com>
2018-05-22 10:24:54 +0200

    easier for debug

--- backend.c
+++ backend.c
@@ -3,21 +3,16 @@
 #include <stdlib.h>
 #include "user_case.h"
 
-#define FILE_RMODE (MPI_MODE_RDONLY)
 #define FILE_NAME "wikipedia_test_small.txt"
 #define BLOCKSIZE 100
 //debug configuration
-#define DEBUG_SCATTER 0
-#define DEBUG_MAP 0
-#define DEBUG_ALL2ALL 1
 
 int main(int argc, char** argv){
-    //get rank or other head operations
-    int rank, num_ranks;
-    char* buff;
-    char* recver;
 
     MPI_Init(&argc, &argv);
+  
+    //get rank or other head operations  
+    int rank, num_ranks;
     MPI_File fh;
     MPI_Offset file_size, file_count;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -25,206 +20,117 @@ int main(int argc, char** argv){
 
     /***Initial and boardcast phase***/
     if(rank == 0){
-        MPI_File_open(MPI_COMM_SELF, FILE_NAME, FILE_RMODE,MPI_INFO_NULL,&fh);
+        MPI_File_open(MPI_COMM_SELF, FILE_NAME, MPI_MODE_RDONLY,MPI_INFO_NULL,&fh);
         //--- It would be smart to include a test in case we fail to open the file ---//
         MPI_File_get_size(fh, &file_size);
     }
     MPI_Bcast(&file_size, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
     //printf("Size is %lld.\n", file_size);
-
-    /***Local map and reduce phase***/
-    int count;
+    
+    //variable for input and scatter
+    char* buff = new char[BLOCKSIZE*num_ranks];
+    char* recver = new char[BLOCKSIZE];
+    
+    //variable for map and shift
     int block_count;
     int count_words=0;
-    int prev_block_count;
-
-    //variable for input and scatter
-    buff = new char[BLOCKSIZE*num_ranks];
-    recver = new char[BLOCKSIZE];
-
-    //variable for map and shift
-
-    //variable for all to all
-    int* rankRecord;
-    KEYVAL* allSend;
-    KEYVAL* allrecv = new KEYVAL[BLOCKSIZE/2];
     KEYVAL* words = new KEYVAL[file_size/(2*(num_ranks-1))];  //Worst case scenario is there are one new word every two characters
-    int* allcount = new int[num_ranks];
-    int* alllimit = new int[num_ranks];         //use reduce for optimized performance
-    int* alldisp = new int[num_ranks];
-    int* alldispr = new int[num_ranks];
+    
+    //variable for all to all
+    int cntCnt;
+    int cntDsp;
+    int* rankRecord;
+    KEYVAL* ataSend;
+    KEYVAL* atarecv = new KEYVAL[BLOCKSIZE/2];
+  
+    int* ataSendCnt = new int[num_ranks];
+    int* ataRecvCnt = new int[num_ranks];         //use reduce for optimized performance
+    int* ataSendDsp = new int[num_ranks];
+    int* ataRecvDsp = new int[num_ranks];
     for(int i=0;i < num_ranks;i++){
-        *(alldispr+i) = i*BLOCKSIZE/2;
-        *(alllimit+i) = BLOCKSIZE/2;
+        *(ataRecvDsp+i) = i*BLOCKSIZE/2;
+        *(ataRecvCnt+i) = BLOCKSIZE/2;
     }
-    //defining datatype for all to all
+    //defining datatype for all to all 
+    KEYVAL sample;
     MPI_Datatype MPI_KEYVAL;
-    MPI_Datatype type[2] = { MPI_INT, MPI_CHAR};
-    int blocklen[2] = { 1, 20};
-    MPI_Aint disp[2];
-    disp[0] = (MPI_Aint) &(allSend->val) - (MPI_Aint) allSend;
-    disp[1] = (MPI_Aint) &(allSend->key) - (MPI_Aint) allSend;
+    MPI_Datatype type[3] = { MPI_INT, MPI_INT, MPI_CHAR};
+    int blocklen[3] = { 1,1,100};
+    MPI_Aint disp[3];
+    disp[0] = (MPI_Aint) &(sample.key_len) - (MPI_Aint) &sample;
+    disp[1] = (MPI_Aint) &(sample.val) - (MPI_Aint) &sample;
+    disp[2] = (MPI_Aint) (sample.key) - (MPI_Aint) &sample;
     MPI_Type_create_struct(2, blocklen, disp, type, &MPI_KEYVAL);
     MPI_Type_commit(&MPI_KEYVAL);
-
-    #if DEBUG_SCATTER
-        char* outbuf = new char[BLOCKSIZE+1];
-    #endif
+    
     file_count = 0;
     while(file_count < file_size - BLOCKSIZE){  //while loop is containing all to all communication now, finish gathering data from all-to-all to proceed another iteration
         /***input and scatter phase***/
         if(rank == 0){
             MPI_File_read(fh,buff+BLOCKSIZE,BLOCKSIZE*(num_ranks - 1),MPI_UNSIGNED_CHAR,MPI_STATUS_IGNORE);
         }
-        MPI_Scatter(buff, BLOCKSIZE, MPI_UNSIGNED_CHAR, recver, BLOCKSIZE, MPI_CHAR, 0, MPI_COMM_WORLD);
-        #if DEBUG_SCATTER
-            if(rank != 0){
-                for(int i=0;i<BLOCKSIZE;i++){
-                    if(*(recver+i) == '\n' ){
-                        outbuf[i] = 'E';
-                    }else if(*(recver+i) == '\t'){
-                        outbuf[i] = 'T';
-                    }else{
-                        outbuf[i] = *(recver+i);
-                    }
-                }
-                outbuf[BLOCKSIZE] = '\0';
-                printf("rank %d receiving: %s \n", rank, outbuf);
-            }
-        #endif
-        #if DEBUG_MAP
-            if (rank==1)    //Debug on process 1
-            {
-                fflush(stdout);
-                // Have a simple visualization of what Map does
-                for(int j=0;j<BLOCKSIZE;j++) printf("%c",recver[j]);
-                printf(" - ");
-                // Comment the above two lines and uncomment the below lines to have a deeper vision of the work done by Map
-                //     printf("Block processed: ");
-                //     for(int j=0;j<BLOCKSIZE;j++) printf("%c",recver[j]);
-                //     printf("\n");
-                //     printf("Word(s) exctracted: ");
-                //     block_count=0;
-                //     while (block_count<BLOCKSIZE){
-                //         prev_block_count = block_count;
-                //         Map(recver,BLOCKSIZE,block_count,&words[0]);
-                //         if(prev_block_count!=0 && words[0].key_len!=0) printf ("  ---  ");
-                //         for(int k=0;k<words[0].key_len;k++) printf("%c",words[0].key[k]);
-                //     }
-                //     printf("\n\n");
-                //     if(words[0].key_len!=0) delete[] words[0].key;
-            }
-        #endif
-
+        MPI_Scatter(buff, BLOCKSIZE, MPI_UNSIGNED_CHAR, recver, BLOCKSIZE, MPI_CHAR, 0, MPI_COMM_WORLD);    
+        
         /***mapping and shift phase***/
+        block_count=0;
+        count_words=0;
         if (rank!=0)
         {
-            block_count=0;
             while(block_count<BLOCKSIZE){
                 words[count_words].key_len=0;
                 Map(recver,BLOCKSIZE,block_count,&words[count_words]);
                 if(words[count_words].key_len!=0) count_words++;     //if we have mapped a word, increment count_words
             }
-        }
+        }            
         //advance offset
         file_count = file_count +  BLOCKSIZE*(num_ranks - 1);
         if(rank==0){
             MPI_File_seek(fh, file_count, MPI_SEEK_SET);
         }
-    }
-
-        /***all to all comm. phase***/
-        allSend = new KEYVAL[count_words];
+        
+        /***all to all comm. phase***/      
+        ataSend = new KEYVAL[count_words];
         rankRecord = new int[count_words];
         for(int i=0;i<count_words;i++){
             rankRecord[i] = calculateDestRank(words[i].key, words[i].key_len, num_ranks);
         }
-        count = 0;
+
+        cntCnt = 0;
+        cntDsp = 0;
         for(int i=0;i<num_ranks;i++){
-            block_count = 0;
-            alldisp[i] = count;
+            cntCnt = 0;
+            ataSendDsp[i] = cntDsp;
             for(int j=0;j<count_words;j++){
                 if(rankRecord[j] == i){
-                    block_count++;
-                    allSend[count] = words[j];
-                    count++;
+                    ataSend[cntDsp] = words[j];
+                    cntCnt++;
+                    cntDsp++;                
                 }
             }
-            allcount[i] = block_count;
-        }
-
-        // Send to and receive from the other processes the amount of data that is going to be sent/received
-        MPI_Alltoall(allcount,1,MPI_INT,alllimit,1,MPI_INT,MPI_COMM_WORLD);
-        // Calculate the new displacement and store it in alldispr
-        count=0;
-        for(int i=0;i<num_ranks;i++){
-            count+=alllimit[i];
-            alldispr[i]=count;
-        }
-
-        for(int i=0;i<num_ranks;i++){
-            printf("%d, %d, %d, %d \n", allcount[i], alldisp[i], alllimit[i], alldispr[i]);
-        }
-        printf("%d \n", rank);
-
-        printf("hello1, rank: %d\n",rank);
-        MPI_Alltoallv(  allSend, allcount, alldisp,
+            ataSendCnt[i] = cntCnt;
+        }  
+        
+        
+        MPI_Alltoallv(  ataSend, ataSendCnt, ataSendDsp,
                         MPI_KEYVAL,
-                        allrecv,alllimit,alldispr,
+                        atarecv, ataRecvCnt, ataRecvDsp,
                         MPI_KEYVAL,
-                        MPI_COMM_WORLD);
-        printf("hello2, rank: %d\n", rank);
-        #if DEBUG_ALL2ALL
-        for(int i=0;i<num_ranks*BLOCKSIZE/2;i++){
-            printf("rank=%d, i=%d, (allrecv+i)->key_len=%d\n", rank, i,(allrecv+i)->key_len);
-            // if((allrecv+i)->key_len>0){
-            //     printf("<");
-            //     for(int j=0;j<(allrecv+i)->key_len;j++) printf("%c", (allrecv+i)->key[j]);
-            //     printf(",");
-            //     printf("%d",(allrecv+i)->val);
-            //     printf("> \n");
-            // }
-        }
-        #endif
-    // }
-
-    #if DEBUG_MAP
-        // Show the mapped data by process 1
-        if (rank==1)
-        {
-            printf("\n\n\nWords mapped by process 1:\n");
-            for(int i=0;i<count_words;i++){
-                for(int j=0;j<words[i].key_len;j++) printf("%c",words[i].key[j]);
-                printf("  -  ");
+                        MPI_COMM_WORLD);  
+          
+        if(rank == 2){
+            for(int i=0;i<num_ranks;i++){
+                printf("for rank %d \n", i);
+                for(int j=0;j<ataSendCnt[i];j++)
+                print_KEYVAL((ataSend+ataSendDsp[i]+j));
             }
-            printf("\n");
         }
-    #endif
+                             
+    }
 
-    //use gather to acquire result
+    delete [] buff;
+    delete [] recver;
+    delete[] words;
     MPI_Finalize();
-
-    #if DEBUG_SCATTER
-        delete [] outbuf;
-    #endif
-
-    // delete [] buff;
-    // delete [] recver;
-    //
-    // /*
-    // till now, <key, value> should be stored in each process.
-    // They are orgainized according to target process in combine phase.
-    // */
-    // //data type to be specified
-    //
-    // /***combine phase***/
-    // //initialize arrays for all to all transmission
-    // //call all to all
-    //
-    // //call reduce
-    //
-    // // Free the mapped data
-    // delete[] words;
 
     return 0;
 }
--- user_case.h
+++ user_case.h
@@ -1,5 +1,6 @@
 #include <stdint.h>
-#define WORD_LEN 20
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifndef USER_CASE
 #define USER_CASE
@@ -11,22 +12,22 @@ const unsigned short *key_seed_num = (unsigned short*)key_seed;
 int calculateDestRank(char *word, int length, int num_ranks)
 {
     uint64_t hash = 0;
-
+    
     for (uint64_t i = 0; i < length; i++)
     {
         uint64_t num_char = (uint64_t)word[i];
         uint64_t seed     = (uint64_t)key_seed_num[(i % SEED_LENGTH)];
-
+        
         hash += num_char * seed * (i + 1);
     }
-
+    
     return (int)(hash % (uint64_t)num_ranks);
 }
 
 typedef struct KEYVAL{      //changed
-    int key_len = 0;
+    int key_len;
     int val;
-    char key[WORD_LEN];
+    char key[100];          //may be we need deep copy for memory efficiency
 } KEYVAL;
 
 
@@ -58,12 +59,12 @@ void Map(char* task, int task_len, int &task_count, KEYVAL *word) //changed, wil
       i++;
     }
     if (isLetter(task[i])){
-        while (isLetter(task[i+word->key_len]) && i+word->key_len<task_len && word->key_len<WORD_LEN) word->key_len++;
+        while (isLetter(task[i+word->key_len]) && i+word->key_len<task_len) word->key_len++;
         //word->key =  new char[word->key_len];
         for(int j=0;j<word->key_len;j++) word->key[j]=task[i+j];
     }
     if (isDigit(task[i])){
-        while (isDigit(task[i+word->key_len]) && i+word->key_len<task_len && word->key_len<WORD_LEN) word->key_len++;
+        while (isDigit(task[i+word->key_len]) && i+word->key_len<task_len) word->key_len++;
         //word->key = new char[word->key_len];
         for(int j=0;j<word->key_len;j++) word->key[j]=task[i+j];
     }
@@ -71,4 +72,14 @@ void Map(char* task, int task_len, int &task_count, KEYVAL *word) //changed, wil
     word->val=1;
 }
 
+void print_KEYVAL(KEYVAL* word){
+    char outbuf[100];
+    for(int i=0;i<word->key_len;i++){               
+        outbuf[i] = word->key[i];
+    }
+    if(word->key_len<100)
+        outbuf[word->key_len] = '\0';
+    outbuf[100] = '\0';
+    printf("    <%s, %d> of len %d \n", outbuf, word->val, word->key_len);      
+}
 #endif
