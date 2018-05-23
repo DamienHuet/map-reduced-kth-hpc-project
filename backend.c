a33026f93b8cd0eb68a8251500432c5ec7ea171e
DamienHuet <damien.huet@protonmail.com>
2018-05-23 17:09:38 +0200

    Fixed the reading length bug: now the file is read until the number of characters remaining is smaller than the number of processes. In process of being implemented: showing the progression of the program when it runs.

--- backend.c
+++ backend.c
@@ -6,10 +6,12 @@
 #include "toolbox.h"
 
 #define FILE_NAME "../test_files/wikipedia_test_small.txt"
-#define BLOCKSIZE 100
+#define BLOCKSIZE 1000
 //debug configuration
 #define DEBUG_ALL2ALL 0
+
 #define SORT_RESULT 1
+#define SHOW_PROGRESS 0
 #define SHOW_RESULT 1
 
 int main(int argc, char** argv){
@@ -30,7 +32,6 @@ int main(int argc, char** argv){
         MPI_File_get_size(fh, &file_size);
     }
     MPI_Bcast(&file_size, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
-    if (rank==0) printf("File size is %lld bytes.\n", file_size);
 
     //variable for input and scatter
     char* buff = new char[BLOCKSIZE*num_ranks];
@@ -78,29 +79,51 @@ int main(int argc, char** argv){
 
     file_count = 0;
     count_words= 0;
-    while(file_count < file_size - BLOCKSIZE){  //while loop is containing all to all communication now, finish gathering data from all-to-all to proceed another iteration
+    int read_size=BLOCKSIZE;
+    if (file_count >= file_size-(num_ranks-1)*read_size){
+        read_size=((file_size-file_count)/(num_ranks-1));
+    }
+    #if SHOW_PROGRESS
+    // if (rank==0) printf("Reading file step:\n");
+    #endif
+    while((file_count < file_size - (num_ranks-1)*read_size) && read_size!=0){
+        #if SHOW_PROGRESS
+        // if (rank==0){
+        //     printf("%d\n",((int)file_size)/((int)file_count));
+            // for(int j=0;j<10;j++){
+            //     if ((file_size/file_count >= 10*j) && (file_count<10*j*(file_size/file_count)+read_size*(num_ranks-1))) printf(" %d -",10*j);
+            // }
+        // }
+        #endif
         /***input and scatter phase***/
         if(rank == 0){
-            MPI_File_read(fh,buff+BLOCKSIZE,BLOCKSIZE*(num_ranks - 1),MPI_UNSIGNED_CHAR,MPI_STATUS_IGNORE);
+            MPI_File_read(fh,buff+read_size,read_size*(num_ranks - 1),MPI_UNSIGNED_CHAR,MPI_STATUS_IGNORE);
         }
-        MPI_Scatter(buff, BLOCKSIZE, MPI_UNSIGNED_CHAR, recver, BLOCKSIZE, MPI_CHAR, 0, MPI_COMM_WORLD);
+        MPI_Scatter(buff, read_size, MPI_UNSIGNED_CHAR, recver, read_size, MPI_CHAR, 0, MPI_COMM_WORLD);
 
         /***mapping and shift phase***/
         block_count=0;
         if (rank!=0)
         {
-            while(block_count<BLOCKSIZE){
+            while(block_count<read_size){
                 words[count_words].key_len=0;
-                Map(recver,BLOCKSIZE,block_count,words+count_words);
+                Map(recver,read_size,block_count,words+count_words);
                 if(words[count_words].key_len!=0) count_words++;     //if we have mapped a word, increment count_words
             }
         }
         //advance offset
-        file_count = file_count +  BLOCKSIZE*(num_ranks - 1);
+        file_count = file_count +  read_size*(num_ranks - 1);
         if(rank==0){
             MPI_File_seek(fh, file_count, MPI_SEEK_SET);
+            }
+        if (file_count >= file_size-(num_ranks-1)*read_size){
+            read_size=((file_size-file_count)/(num_ranks-1));
         }
     }   //--- End of while loop ---//
+    if (rank==0){   //Printf reading informations
+         printf("File size is %lld bytes. ", file_size);
+         printf("Number of unread characters (at the end of the file): %lld\n",file_size-file_count);
+    }
 
     delete [] buff, recver;
 
@@ -179,7 +202,11 @@ int main(int argc, char** argv){
     }
     delete [] atarecv;
 
-    // To be implemented
+    #if SORT_RESULT
+    // If we want to perform the sort in a parallel way (not implemented yet: to be implemented when the std::map will be available)
+        // First, sort the lists on each process (sequentially here, but why not using OpenMP)
+        // quickSort(reduceAry,0,reduceNb-1);
+    #endif
 
     // Gather the reduced result on master
     TotNbWords=0;
@@ -192,7 +219,7 @@ int main(int argc, char** argv){
             gatherRecvDsp[i]=TotNbWords;
             TotNbWords+=gatherRecvCnt[i];
         }
-        printf("There are %d different words:\n",TotNbWords);
+        printf("There are %d different words.\n",TotNbWords);
         gatherRcv = new KEYVAL[TotNbWords];
     }
     MPI_Gatherv(reduceAry,LocNbWords,
@@ -201,6 +228,7 @@ int main(int argc, char** argv){
                     MPI_KEYVAL,0,MPI_COMM_WORLD);
 
     #if SORT_RESULT
+        // If we do serial sort instead of parallel sort...
         if (rank==0){
             quickSort(gatherRcv,0,TotNbWords-1);
         }
